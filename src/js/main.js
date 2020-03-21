9d784f83be94bf9ad640492f7f1d92b220e2ad05
= <=>
2020-03-21 18:43:10 +0300

    as everal updates

--- src/js/main.js
+++ src/js/main.js
@@ -2,10 +2,41 @@
     var method = {
         search: document.querySelector('.search-input'),
         delta: document.querySelector('.label-input div'),
-        active: 'active'
+        button: document.querySelector('.button-submit'),
+        span: document.querySelector('.button-submit span'),
+        active: 'active',
+        button_active: 'submit-active'
     }; 
-    
- 
+
+    function add(feature) 
+    {
+        feature.classList.add(method.button_active);
+    }
+
+    method.button.addEventListener('click', (e)=> {
+        e.preventDefault();
+
+        var mValue = Math.max(method.button.clientX, method.button.clientY); 
+
+        var getFeature = method.button.getBoundingClientRect();
+        setTimeout(function() {
+            method.span.style.width=`110px`; 
+            method.span.style.height = `110px`;
+            method.span.style.transform=`translate(-50%, -50%) scale(2)`;
+            method.span.style.opacity="0";
+            method.span.style.transition="0.4s ease";
+        }, 100);
+
+        method.span.style.left = `${e.clientX - getFeature.left}px`;
+        method.span.style.top = `${e.clientY - getFeature.top}px`;
+        method.span.style.transition="0s";
+        method.span.style.transform=`translate(-50%, -50%)`;
+        method.span.style.width=`0px`; 
+        method.span.style.height = `0px`;
+
+        method.span.style.opacity="1";
+    });
+
     if ((typeof method.search.value !== 'string')) 
     {
         String(method.search.value); 
@@ -72,6 +103,7 @@
         }
       
     });
+
 
 
 }).call(this);
