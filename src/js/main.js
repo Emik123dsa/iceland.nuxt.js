acb7e128ee71bc2e973f43a63e6b6eff5bee50f9
= <=>
2020-03-21 19:57:06 +0300

    new updates as usual

--- src/js/main.js
+++ src/js/main.js
@@ -4,6 +4,7 @@
         delta: document.querySelector('.label-input div'),
         button: document.querySelector('.button-submit'),
         span: document.querySelector('.button-submit span'),
+        form: document.querySelectorAll('.hero_wrap_header_form'),
         active: 'active',
         button_active: 'submit-active'
     }; 
@@ -12,6 +13,28 @@
     {
         feature.classList.add(method.button_active);
     }
+
+    Array.prototype.forEach.call(method.form, function(e) 
+    {
+        e.addEventListener('mousemove', function(e) 
+        { 
+            this.style.transition="1s ease";
+            this.style.transform=`translate(${e.offsetX/15}px, ${e.offsetY/15}px)`;
+        });
+        e.removeEventListener('mousemove', function(e) 
+        { 
+            this.style.transition="1s ease";
+            this.style.transform=`translate(0px, 0px)`;
+        });
+
+        e.removeEventListener('mouseout', function(e) 
+        { 
+            this.style.transition="0";
+            this.style.transform=`translate(0px, 0px)`;
+        });
+
+    });
+   
 
     method.button.addEventListener('click', (e)=> {
         e.preventDefault();
@@ -25,15 +48,15 @@
             method.span.style.transform=`translate(-50%, -50%) scale(2)`;
             method.span.style.opacity="0";
             method.span.style.transition="0.4s ease";
-        }, 100);
-
+            method.span.style.zIndex="2";
+        }, 30);
+        method.span.style.zIndex="-2";
         method.span.style.left = `${e.clientX - getFeature.left}px`;
         method.span.style.top = `${e.clientY - getFeature.top}px`;
         method.span.style.transition="0s";
         method.span.style.transform=`translate(-50%, -50%)`;
         method.span.style.width=`0px`; 
         method.span.style.height = `0px`;
-
         method.span.style.opacity="1";
     });
 
